149d0c4d338ff16e92353576a1fc8856478fbb7e
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-19 21:30:06 -0500

    fixed bugs, finished step 5
    corrected typo in the getComputerChoice() function:
    choice === "scissors" --> choice = "scissors"
    
    added a return statement to the getHumanChoice() function
    
    fixed bug in playRound() function where rock beat paper instead of scissors
    
    cleaned up logic in playRound() function:
    - changed the logic for the if statement handling the tie condition to
      humanChoice === computerChoice instead of typing out each possible choice pair

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
     } else if (random === 1) {
         choice = "paper";
     } else if (random === 2) {
-        choice === "scissors";
+        choice = "scissors";
     }
 
     console.log("computer choice: " + choice);
@@ -17,25 +17,28 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("choice?");
-
     console.log("human choice: " + humanChoice);
+    return humanChoice;
 }
 
 let computerScore = 0;
 let humanScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    let statement;
-    if ((humanChoice === "rock" && computerChoice === "rock") ||
-        (humanChoice === "paper" && computerChoice === "paper") ||
-        (humanChoice === "scissors" && computerChoice === "scissors")) {
+    let statement = "";
+    // tie conditions
+    if (humanChoice === computerChoice) {
             statement = "Tie!";
-        } else if ((humanChoice === "rock" && computerChoice === "paper") ||
+        } 
+        // win conditions
+        else if ((humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")) {
             statement = "You win! " + humanChoice + " beats " + computerChoice + "!";
             humanScore++;
-        } else if ((humanChoice === "rock" && computerChoice === "paper") ||
+        } 
+        // lose conditions
+        else if ((humanChoice === "rock" && computerChoice === "paper") ||
         (humanChoice === "paper" && computerChoice === "scissors") ||
         (humanChoice === "scissors" && computerChoice === "rock")) {
             statement = "You lose. " + computerChoice + " beats " + humanChoice + "!";
